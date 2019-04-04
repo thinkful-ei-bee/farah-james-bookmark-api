a015145f9ab5cc967f9d7c0e2748938f9daa6553
Farah Student <farah.gus@hotmail.com>
2019-04-04 14:40:27 -0400

    api key handling middleware

--- src/app.js
+++ src/app.js
@@ -30,6 +30,26 @@ app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
 
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
+})
+
+const bookmarks = [{
+  id: 1,
+  title: 'Google',
+  description: 'Search engine',
+  url: 'https://www.google.com',
+  rating: 5
+}];
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
