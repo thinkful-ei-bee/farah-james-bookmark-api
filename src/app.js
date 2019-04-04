755e55bc311bf466bc7bfcb60a05447b9b727e1b
Farah Student <farah.gus@hotmail.com>
2019-04-04 14:34:22 -0400

    install winston

--- src/app.js
+++ src/app.js
@@ -4,8 +4,23 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const {NODE_ENV} = require("./config");
+const winston = require('winston');
 
 const app = express();
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'info.log' })
+  ]
+});
+
+if (NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
 
 const morganOption = NODE_ENV === "production"
   ? "tiny"
