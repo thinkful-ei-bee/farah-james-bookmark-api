64441f9fcef2dcab6a99f4c14f518c987ecae856
likejackonfire <gravitybringsmedown@gmail.com>
2019-04-04 15:37:41 -0700

    added API_TOKEN

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
+const logger = require("./logger");
 const {NODE_ENV} = require("./config");
 const bookmark_Router = require('./bookmarks/bookmark-router');
 
@@ -16,16 +17,14 @@ const morganOption = NODE_ENV === "production"
 app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
-
+console.log(bookmark_Router)
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
   const authToken = req.get('Authorization')
-
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     logger.error(`Unauthorized request to path: ${req.path}`);
     return res.status(401).json({ error: 'Unauthorized request' })
   }
-  // move to the next middleware
   next()
 })
 
